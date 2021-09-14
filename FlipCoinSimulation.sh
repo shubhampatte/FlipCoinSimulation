6dbe6a956e1943935ac9ce6416899a4e3f57d7a0
shubhampatte <shubhampatte@gmail.com>
2021-09-14 20:09:21 +0530

    number of times head and tail

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,12 +1,24 @@
 #! /bin/bash -x
+
 echo "Program of flip coin simulation"
 
-toss=$(($RANDOM%2))
+read -p "enter number of times coin is flip :" num
 
-if [ $toss -eq 1 ]
-then
-	echo "HEAD"
-else
-	echo "TAIL"
+head=0
+tail=0
+
+for ((i=1;i<=$num;i++))
+do
+	echo "diplaying heads or tails"
+
+	toss=$(($RANDOM%2))
+
+	if [ $toss -eq 1 ]
+	then
+		echo "HEAD"
+	else
+		echo "TAIL"
 fi
-
+done
+echo "number of heads :" $head
+echo "number of tails :" $tail
